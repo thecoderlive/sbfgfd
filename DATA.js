4194aeff8b7cb7eafd90f843a9756a812a244566
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-10 12:44:38 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "12da1341-03fc-4ae7-8f4a-faf95c428bd0",
+    "home_feed": [
+        {
+            "id": "bf1c0e59-0569-401a-ae43-ef972a77c65c",
+            "artist_name": "if",
+            "home_carousel": [
+                {
+                    "id": "1cb43d0b-622e-4f1d-8334-d0abfde20aaf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/181.png"
+                },
+                {
+                    "id": "305a4ee0-4c4a-46a9-bfed-1ff043fe7fde",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/349.png"
+                },
+                {
+                    "id": "cd42bc44-7e35-4fd2-9a7e-9d9684fb530d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/11.png"
+                },
+                {
+                    "id": "a2b55a0c-4f6b-487e-9a55-70762f75befa",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/345.png"
+                },
+                {
+                    "id": "36c469b1-4300-45d5-b5f3-77041cecc51a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/268.png"
+                }
+            ],
+            "art_caption": "trade",
+            "art_description": "That hundred memory seem. Culture music choice investment better turn fill. Individual method capital especially boy."
+        },
+        {
+            "id": "64839cc7-719e-47b7-b422-d7a37bed93b0",
+            "artist_name": "send",
+            "home_carousel": [
+                {
+                    "id": "9acee514-a755-4aa1-b511-e759607ead8a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/322.png"
+                },
+                {
+                    "id": "16d92ffd-640f-4941-82a9-8e9132e52616",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/204.png"
+                },
+                {
+                    "id": "52672e7c-df0d-45ae-8b70-3f8a002b2b0d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/308.png"
+                },
+                {
+                    "id": "a6a94dd9-c636-420a-b375-fce94161d649",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/171.png"
+                },
+                {
+                    "id": "f1130f4c-b5ce-4d86-a5de-457786df7450",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/303.png"
+                }
+            ],
+            "art_caption": "total",
+            "art_description": "Poor recently child happy. Worry evidence turn own a town. Weight true scientist include social."
+        },
+        {
+            "id": "2bf64909-425f-4fc4-ba88-46707047a158",
+            "artist_name": "head",
+            "home_carousel": [
+                {
+                    "id": "3a925131-cd6d-4ce2-afa0-38fb8d6abeb3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/242.png"
+                },
+                {
+                    "id": "a44d8453-e81c-427a-a75f-166b79c59420",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/267.png"
+                },
+                {
+                    "id": "28632ec7-b25c-454e-92f3-cfc64dbd40be",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/231.png"
+                },
+                {
+                    "id": "d575b94c-361b-47bf-9aa6-5f3b35bbacd3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/299.png"
+                },
+                {
+                    "id": "56c7ae06-e411-4e5e-a7e8-6ee4969ae7a9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/65.png"
+                }
+            ],
+            "art_caption": "herself",
+            "art_description": "Feeling baby choose report. Instead building full receive police position. Your wife myself itself than."
+        },
+        {
+            "id": "7290e880-d6a7-4f49-98de-1422c048b2b7",
+            "artist_name": "night",
+            "home_carousel": [
+                {
+                    "id": "8d99e3dd-d9eb-4cbe-b252-ba1ec659b349",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/178.png"
+                },
+                {
+                    "id": "43c1445b-923b-4347-abc7-108fefd2c4e1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/23.png"
+                },
+                {
+                    "id": "bbe0f538-1896-4cf7-bb22-da05b8f2a1d3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/11.png"
+                },
+                {
+                    "id": "4290e699-a99f-4fdd-bf2c-504485188a9c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/84.png"
+                },
+                {
+                    "id": "8f1b6ba4-7645-4810-bb7b-42df5be8680c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/243.png"
+                }
+            ],
+            "art_caption": "break",
+            "art_description": "Usually voice maybe same. Garden not provide arm leave perform. Value some score by break."
+        },
+        {
+            "id": "a3ede59f-c97b-4bf5-92b6-970598918963",
+            "artist_name": "behind",
+            "home_carousel": [
+                {
+                    "id": "cab83625-adc6-4968-b708-8d7ecbc5c5ea",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/170.png"
+                },
+                {
+                    "id": "ab17db3f-5f4f-4fc2-adfd-e1d1fe2e52f8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/287.png"
+                },
+                {
+                    "id": "016c9910-82c4-4aa3-9f33-20f9d17e738f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/177.png"
+                },
+                {
+                    "id": "8f355b45-ad0f-4f5b-84bc-d8f0d60077f2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/86.png"
+                },
+                {
+                    "id": "72c6357c-bd28-49aa-b4b0-9a28e86df413",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/338.png"
+                }
+            ],
+            "art_caption": "seat",
+            "art_description": "The for hair shoulder trade one or bit. Activity all music hospital. Food note window life."
+        }
+    ]
+}
